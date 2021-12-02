f7a810637b272630c5ff89409bbc168ec86a72fe
sobin <thqls425@naver.com>
2021-12-02 22:46:38 +0900

    13주차 실습 완료

---
+++ app/src/main/java/com/example/webrequest_application/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.webrequest_application;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/webrequest_application/MovieListResult.java
@@ -0,0 +1,20 @@
+package com.example.webrequest_application;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+
+}
